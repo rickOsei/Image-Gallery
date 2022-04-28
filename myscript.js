ec54f6e48fea46ad9adc9cd7e21ac399e4cfde84
Osei Richard Ampah <rickoseiampah@gmail.com>
2022-04-28 04:25:15 +0300

    final changes

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function myfunction(){
+    document.getElementById ("main").style.display="none";
+    document.getElementById ("sub").style.display="";
+    document.getElementById ("mainHead").style.color = "#000";
+}
+
+function myfunctionx() {
+    document.getElementById ("main").style.display="";
+    document.getElementById ("sub").style.display="none";
+    document.getElementById ("mainHead").style.color = "#bba14f"
+}
